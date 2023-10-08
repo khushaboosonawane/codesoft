5ab770810d58a221dfaf35ad5faae36adb198101
khushaboosonawane <146915812+khushaboosonawane@users.noreply.github.com>
2023-10-08 08:53:22 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.text', {
+    strings: ['Frontend Developer','Backend Developer','Web Developer','Java Developer','Python Developer'],
+    typeSpeed: 100,
+    backSpeed:100,
+    backDelay:500,
+    loop:true
+  });
